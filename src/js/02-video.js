62b23b6c90ac638291e8b3cc183a83291d14737d
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-05-03 12:56:22 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+var _ = require('lodash');
 
+let timeTrottled = 0;
+
+const onPlay = function (data) {
+  time = data.seconds;
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
+player.on('loaded', () => {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+});
