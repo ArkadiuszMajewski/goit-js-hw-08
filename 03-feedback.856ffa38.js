ca15d7cf0618c053c81a95262036970dd630f49f
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-05-03 11:18:45 +0000

    Deploying to gh-pages from @ ArkadiuszMajewski/goit-js-hw-08@c962e47ee5a9b730c7b34df79d3836a003c8e9f0 🚀

---
+++ 03-feedback.856ffa38.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("submit",(t=>{e.preventDefault()}));
+//# sourceMappingURL=03-feedback.856ffa38.js.map
---
+++ 03-feedback.eb88fcb3.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");console.log(e),e.addEventListener("submit",(function(n){e.preventDefault()}))}();
+//# sourceMappingURL=03-feedback.eb88fcb3.js.map
