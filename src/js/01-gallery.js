6890486ab020f1de02b4a52d1403427560187332
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-05-04 11:17:59 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,33 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from './gallery-items';
 // Change code below this line
-const gallery = document.querySelector(".gallery");
-console.log(galleryItems);
-console.log(SimpleLightbox);
-galleryItems.forEach((galleryItem) => {
-  const li = document.createElement("li");
-  li.classList.add("gallery__item");
-  const a = document.createElement("a");
-  a.classList.add("gallery__link");
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(galleryItem => {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+  const a = document.createElement('a');
+  a.classList.add('gallery__link');
   a.href = `${galleryItem.original}`;
-  const img = document.createElement("img");
-  img.classList.add("gallery__image");
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
   img.src = `${galleryItem.preview}`;
   img.alt = `${galleryItem.description}`;
   img.title = `${galleryItem.description}`;
   gallery.append(li);
   li.append(a);
   a.append(img);
-  li.addEventListener("click", (event) => {
+  li.addEventListener('click', event => {
     event.preventDefault();
   });
 });
 
 (function () {
-  var $gallery = new SimpleLightbox(".gallery a", {});
+  var $gallery = new SimpleLightbox('.gallery a', {});
 
   $gallery.options.captionDelay = 500;
   console.log($gallery.options);
 })();
-gallery.style.listStyle = "none";
+gallery.style.listStyle = 'none';
