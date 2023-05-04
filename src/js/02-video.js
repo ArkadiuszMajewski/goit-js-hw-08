5b3db14aa7758b187601e284b6379014bde16e2c
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-05-04 09:46:54 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 import Player from '@vimeo/player';
-// // const iframe = document.querySelector('iframe');
-// // const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 // const player = new Player('vimeo-player', {});
 // console.log();
 // var throttle = require('lodash.throttle');
