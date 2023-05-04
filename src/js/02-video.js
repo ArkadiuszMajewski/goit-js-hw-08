b6ef9a65931766b9d63b4e66d592edf7d65b0393
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-05-04 08:54:13 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,8 @@
 import Player from '@vimeo/player';
-const iframe = document.querySelector('iframe');
-console.log(iframe);
-const player = new Player('vimeo-player', {
-  id: 19231868,
-  width: 640,
-});
-console.log(player);
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+const player = new Player('vimeo-player', {});
+console.log();
 var throttle = require('lodash.throttle');
 
 const onPlay = function (data) {
@@ -13,8 +10,30 @@ const onPlay = function (data) {
   localStorage.setItem('videoplayer-current-time', time);
 };
 
-player.on('timeupdate', throttle(onPlay, 2000));
+player.on('timeupdate', onPlay);
 
 player.on('loaded', () => {
   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 });
+//
+//
+//
+//
+
+// import Player from '@vimeo/player';
+// // const iframe = document.querySelector('iframe');
+// // const player = new Vimeo.Player(iframe);
+// const player = new Player('vimeo-player', {});
+// console.log();
+// var throttle = require('lodash.throttle');
+
+// const onPlay = function (data) {
+//   time = data.seconds;
+//   localStorage.setItem('videoplayer-current-time', time);
+// };
+
+// player.on('timeupdate', throttle(onPlay, 2000));
+
+// player.on('loaded', () => {
+//   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// });
